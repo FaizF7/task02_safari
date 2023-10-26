25fc35ca5fce802b01e185752191d25e591b0159
Sarah Ogunko <sarahogunko@hotmail.com>
2023-10-26 16:07:56 +0100

    completed staff and assignments tables

--- safari.sql
+++ safari.sql
@@ -1,3 +1,5 @@
+DROP TABLE IF EXISTS assignments;
+DROP TABLE IF EXISTS staff;
 DROP TABLE IF EXISTS animals;
 DROP TABLE IF EXISTS enclosures;
 
@@ -16,13 +18,38 @@ CREATE TABLE animals(
     enclosure_id INT REFERENCES enclosures(id)
 );
 
+CREATE TABLE staff(
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255),
+    employee_number INT
+);
+
+CREATE TABLE assignments(
+    id SERIAL PRIMARY KEY,
+    employee_id INT REFERENCES staff(id),
+    enclosure_id INT REFERENCES enclosures(id),
+    day VARCHAR(255)
+);
+
 INSERT INTO enclosures (name, capacity, closed_for_maintenance) VALUES ('Big cat field', 20, 'false');
-INSERT INTO enclosures (name, capacity, closed_for_maintenance) VALUES ('Tortoise garden', 10, 'false');
+INSERT INTO enclosures (name, capacity, closed_for_maintenance) VALUES ('Tortoise garden', 10, 'true');
 INSERT INTO enclosures (name, capacity, closed_for_maintenance) VALUES ('Shark territory', 4, 'false');
-INSERT INTO enclosures (name, capacity, closed_for_maintenance) VALUES ('Aqauatic centre', 25, 'false');
+INSERT INTO enclosures (name, capacity, closed_for_maintenance) VALUES ('Aqauatic centre', 25, 'true');
 
 INSERT INTO animals(name, type, age, enclosure_id) VALUES ('Tony','Tiger', 59, 1);
 INSERT INTO animals(name, type, age, enclosure_id) VALUES ('Tom','Tortoise', 110, 2);
 INSERT INTO animals(name, type, age, enclosure_id) VALUES ('Katie','Lion', 29, 1);
 INSERT INTO animals(name, type, age, enclosure_id) VALUES ('Jerome','Tiger shark', 36, 3);
 INSERT INTO animals(name, type, age, enclosure_id) VALUES ('Sammy','Seal', 10, 4);
+
+INSERT INTO staff(name, employee_number) VALUES ('Sarah',5456);
+INSERT INTO staff(name, employee_number) VALUES ('Faiz',5826);
+INSERT INTO staff(name, employee_number) VALUES ('Claudia',1556);
+INSERT INTO staff(name, employee_number) VALUES ('Zoe',8956);
+
+
+INSERT INTO assignments(employee_id, enclosure_id, day) VALUES (1,2, 'Monday');
+INSERT INTO assignments(employee_id, enclosure_id, day) VALUES (2,3, 'Tuesday');
+INSERT INTO assignments(employee_id, enclosure_id, day) VALUES (3,4, 'Tuesday');
+INSERT INTO assignments(employee_id, enclosure_id, day) VALUES (4,1, 'Saturday');
+INSERT INTO assignments(employee_id, enclosure_id, day) VALUES (2,4, 'Wednesday');
